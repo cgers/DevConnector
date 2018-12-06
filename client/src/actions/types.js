8c147e3f2e46140df3e2ef33a92dcf2d084f1ce5
craig.gers <craig.gers@EMEAGBRABXLT252>
2018-12-06 12:59:37 +0000

    Post reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,3 +5,10 @@ export const GET_PROFILES = 'GET_PROFILES'; // Get an array of profiles
 export const PROFILE_LOADING = 'PROFILE_LOADING'; // when a profile is loading
 export const PROFILE_NOT_FOUND = 'PROFILE_NOT_FOUND'; // When a profile is not found.
 export const CLEAR_CURRENT_PROFILE = 'CLEAR_CURRENT_PROFILE'; // To chear current profile.
+
+//Types for POST(S)
+export const POST_LOADING = 'POST_LOADING'; // Spinner when post loads
+export const GET_POSTS = 'GET_POSTS'; // To GET all the posts
+export const GET_POST = 'GET_POST'; // To get a single post
+export const ADD_POST = 'ADD_POST'; // To ADD a post.
+export const DELETE_POST = 'DELETE_POST'; // To delete a post
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,18 @@
+import { GET_ERRORS } from '../actions/types';
+
+//Empty object for the errors
+const intitalState = {
+	posts: [],
+	post: {},
+	loading: false
+};
+
+export default function(state = intitalState, action) {
+	//can include payload
+	switch (action.type) {
+		case GET_ERRORS:
+			return action.payload;
+		default:
+			return state;
+	}
+}
